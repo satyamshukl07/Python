808d9d0210b90e03493f6b27af952406c3610a5d
satyam shukla <satyamshukla9528@gmail.com>
2026-04-02 22:38:06 +0530

    Added day 1

---
+++ DAY1/LECTURE.PY.py
@@ -0,0 +1,151 @@
+#print("Hello my name is satyam shukla")
+#print("my age is 23")
+#print("hello world")
+#print("hay guys this is my first vloge of python ")
+#print("its the basic prograam of python")
+#print("i am leaning python because i just wanna learn AI/Ml")
+
+#in same line diffrent text
+#print("hello its my first line and iam just goig to write another line of code in same line" ,"ok so, this is the another line of code.")
+#print(23+3)
+ #a= 10
+ #b= 40
+#sum= a+b
+#print(sum)
+
+#name = "satyam"
+#age = 23
+#price = 23.99
+#print ("my name is" ,name)
+
+#print(type(name))
+#print(type(age))
+#print(type(price))
+
+#basically here iam going to talk about data types in python ok!! so, lets get started
+# first data type is 
+# 1 integers in which we contain -ve and +veb values  that is knowns as integers
+#2 String is the data tyoe in which we conatain name of person place or things
+#3 float float can contain numbers in points like 3.0 and 42.524 ect in which we can also contain +ve anad -ve value
+#4 bollena is the type of container in which we can only contain only truen and false values
+#5 another type of values are contain  none which means it conatain nothing means  nothing
+
+#age = 23 
+#old = False
+#a= None
+#print(type(old))
+
+
+#about keyword
+# there is some keyword in python but u can use there name in your python file u have to another but if u just wanna add thenn u get error
+
+#python is a case senstive language
+
+
+#now you have to pritn sum of two numbers
+
+#a= 10
+#b= 23
+#sum= a+b
+#print(sum)
+
+#this is the simple addtion of two numbers in pyrhon
+
+"""ok now iam going to write mutiline comment foer checking in this things ok now iam applies all lthese things"""
+
+
+ # ok so, you have to write miulti line comments in vs code then you haveb to press ctrl + forword slash / then u can use munlti line comments
+#  now ia m goig to write multi line coment 
+# o KeyboardInterrupt
+# here i write lots of unuse code and going to comment
+
+
+# now iam going to understand the operator 
+#  and there is lots of operator in python 
+# now here iam goin to tell u about the operator
+
+
+# arithmatic oprator
+
+# a= 4
+# b= 23
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a%b)
+ 
+
+ #relational opeator in this operator we have realtion or comariosion with other operator
+# a= 10
+# b= 30
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+ #Assingment operator
+
+ # in assingment operator there is variois types of opersator like 
+ #
+  
+
+ #logical operator
+ #in logical operator we have three operator like 
+ # and or not  these are three operators which is basically use in python
+# 
+
+
+# type conversion
+
+#there is two type of conversion 
+#1. tpye conversion
+
+# a= 2
+# b= 2.03
+# sum = a+b
+# print(sum)
+# #2. 
+
+#input in python
+# name = input("enter your age")
+# input("you enterd23", name)
+
+#print("hello world")
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# marks = int(input("Enter your marks "))
+
+# print("Welcome" , name)
+# print(" Your age is: " , age)
+# print("And Your marks is" , marks)
+# type(name)
+
+#now iam going to do practice questions 
+#queston1
+# first = int(input("Enter your first value"))
+# second = int(input("Enter your second value"))
+# print("sum = ", first+second)
+
+#Question number 2;
+#Print the area of square when u have given the side of square
+# a= float(input("enter the area of side"))
+# print("area of square is: "  , a*a)
+
+#question number 3 wap to print 2 floating points number and print there average
+
+# first = float(input("Enter your first value"))
+# second = float(input("Enter your second value"))
+# print("avg of two numbers" , (first+second)/2)
+
+#Question number 3 WAP to input 2 int numbers, a and b.
+#print true if a is greater then b or equal to be if not print false
+
+a= int(input("Enter your first value"))
+b= int(input("Enter your second value"))
+print (a>=b)
+
+
