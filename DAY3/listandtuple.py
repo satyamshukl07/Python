ac7c6a401a3c7be1dcdefcc8ca46d58576b42ec1
satyam shukla <satyamshukla9528@gmail.com>
2026-04-06 19:50:20 +0530

    added day 3

---
+++ DAY3/listandtuple.py
@@ -0,0 +1,63 @@
+# marks = [22.86,32.65,44.56,23.65,54.75,24.63]
+# print(marks)
+# print(type(marks))
+# print(marks[1])
+# print(marks[4])
+
+
+# list slicing there is slicing in list also this is a very good function of python 
+
+#there is some list funtion in list method okk so, now iam going to study all these things
+# like 
+
+# list = [2,1,3]
+# list.append(4)  adds one element at the end  [2,1,3,4]
+# list.sort()  assending order [1,3,4,6]
+# list.sort(reverse = True) short in desending order[3,2,1]
+# list.reverse()[3,1,2]
+# list.insert(idx,el)
+
+# list = [1,43,52,522,66,34,78]
+# list.append(4)
+# print(list)
+# 
+# print(list.sort())
+# print(list)
+
+# print(list.sort(reverse = True))
+# print(list)
+# print(list.reverse())
+# print(list) 
+# list.insert(52,70)
+# print(list)
+
+# list.remove(1)
+# print(list)
+
+# list.pop(3)
+# print(list)
+
+                                    #tuples in python
+
+    #a built in data type that lets us create immutable sequence of values.
+# tup = (87,64,33,95,76)
+# print(type(tup))
+# print(tup[0])
+# print(tup[1])
+
+
+                #tupels k under bhi slising hoti hai
+
+# tup = (2,1,4,3,5,4)
+# print(tup.index(4))
+ 
+movies =[]
+mov1 = input("enter 1st movie: ")
+mov2 = input("enter 2nd movie: ")
+mov3 = input("enter 3rd movie: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
